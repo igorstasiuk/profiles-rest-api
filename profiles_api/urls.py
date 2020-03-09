d3558bdd883e22185acfec7c1125b8ddc6c884a4
igorstasiuk <obvilion08@gmail.com>
2020-03-09 16:31:22 +0300

    Added post, put, patch and delete to our APIView

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,6 @@
-from django.urls import  path
+from django.urls import path
 
-from profiles_api import  views
+from profiles_api import views
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view())
